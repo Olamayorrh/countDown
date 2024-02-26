8e5e17c63ccfecc573ea2d37a635eea327828eeb
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-02-26 23:27:35 +0100

    Update index.js

--- countdownTimer/index.js
+++ countdownTimer/index.js
@@ -1,4 +1,4 @@
-let targetDate = new Date("2024-02-28 00:00:00").getTime();
+let targetDate = new Date("2024-05-28 00:00:00").getTime();
 console.log(targetDate);
 
 let launch = setInterval(function(){
